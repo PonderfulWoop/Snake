2139166564706abcb3e5f56645bb6a8b04cea9ea
Shashank Shirol <shashank.shirol1@gmail.com>
2019-04-02 19:40:58 +0530

    2-4-19; 19:40

--- snake.py
+++ snake.py
@@ -6,7 +6,7 @@ from tkinter import messagebox
 
 class cube(object):
     rows = 20
-    w = 500
+    w = 650
     def __init__(self, start, dirnx = 1, dirny = 0,  color = (255, 0, 0)):
         self.pos = start
         self.dirnx = 1
@@ -130,9 +130,6 @@ def drawGrid(w, rows, surface):
         x = x + sizebtwn
         y = y + sizebtwn
 
-        pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, w))
-        pygame.draw.line(surface, (255, 255, 255), (0, y), (w, y))
-
 def redrawWindow(surface):
     global rows, width, s, snack
     surface.fill((0, 0, 0))
@@ -167,7 +164,7 @@ def message(subject, content):
 def main():
     global width, rows, s, snack
 
-    width = 500
+    width = 650
     rows = 20
     win = pygame.display.set_mode((width, width))
     pygame.display.set_caption('Snake v0.0.0.1')
@@ -184,10 +181,14 @@ def main():
         if s.body[0].pos == snack.pos:
             s.addCube()
             snack = cube(randomSnack(rows, s), color=(0,255,0))
+            pygame.display.set_caption(
+                'Snake v0.0.0.1 || Score: ' + str(len(s.body)))
         for x in range(len(s.body)):
             if s.body[x].pos in list(map(lambda z: z.pos, s.body[x+1:])):
+                pygame.display.set_caption(
+                    'Snake v0.0.0.1')
                 print('Score: ', len(s.body))
-                message('You Lost!', 'Your Score: ' +
+                message('Game Over', 'Your Score: ' +
                         str(len(s.body))+'\nPlay Again')
                 s.reset((10, 10))
                 break
